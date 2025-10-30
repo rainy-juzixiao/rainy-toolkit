135b103a8af8eb175c87727c4e0f2e6d6301f7ee
rainy juzixiao <aTzXQR8w96I@outlook.com>
2025-10-30 23:46:55 +0800

    10/30/25 rewrite "any transform"

--- xaga/include/rainy/utility/any.hpp
+++ xaga/include/rainy/utility/any.hpp
@@ -752,12 +752,12 @@ namespace rainy::utility {
         template <typename Fx>
         basic_any &transform(Fx &&handler) {
             using namespace type_traits;
-            using type_list = typename other_trans::type_list<typename other_trans::tuple_like_to_type_list<primary_types::param_list_in_tuple<Fx>>::type>::type;
-            using target_type = cv_modify::remove_cvref_t<typename other_trans::type_at<0, type_list>::type>;
+            using type_list = typename other_trans::tuple_like_to_type_list<primary_types::param_list_in_tuple<Fx>>::type;
+            using target_type = typename other_trans::type_at<0, type_list>::type;
             if (is<target_type>()) {
-                basic_any(std::in_place_type<target_type>, handler(this->template as<target_type>())).swap(*this);
+                basic_any(utility::invoke(handler, this->template as<target_type>())).swap(*this);
             } else if (is_convertible<target_type>()) {
-                basic_any(std::in_place_type<target_type>, handler(this->template convert<target_type>())).swap(*this);
+                basic_any(utility::invoke(handler, this->template convert<target_type>())).swap(*this);
             }
             return *this;
         }
