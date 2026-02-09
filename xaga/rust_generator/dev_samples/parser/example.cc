0044a6b62188ed2a99be01d4f44e4f88b7cde9e4
rainy-juzixiao <rainy_juzixiao@outlook.com>
2026-02-09 12:31:38 +0800

    2/9/26
    Fix

---
+++ xaga/rust_generator/dev_samples/parser/example.cc
@@ -0,0 +1,102 @@
+#define RAINY_ENABLE_MOC(...)
+
+#define RAINY_GRANT_ACCESS_NAMESPACE(x)
+#define RAINY_GRANT_ACCESS_USING(x)
+
+class Foo {
+};
+
+class Bar {
+};
+
+class a {
+public:
+    RAINY_ENABLE_MOC()
+
+    a() { }
+
+    void foo() {
+
+    }
+
+    void foo(int, int) {
+    }
+
+    int mem() {
+        return 0;
+    }
+
+    static void bar() {}
+
+    int aa;
+
+private:
+    int b;
+};
+
+class b {
+public:
+    RAINY_ENABLE_MOC(
+        RAINY_GRANT_ACCESS_NAMESPACE("xxx"),
+        RAINY_GRANT_ACCESS_NAMESPACE("x1xx"),
+        RAINY_GRANT_ACCESS_USING("xxx")
+    )
+
+    b() { }
+
+    void foo() {
+
+    }
+
+    void foo(int, int) {
+
+    }
+
+    static void bar() {}
+
+    int field;
+};
+
+RAINY_ENABLE_MOC(
+    RAINY_GRANT_ACCESS_NAMESPACE("xxx"),
+    RAINY_GRANT_ACCESS_USING("xxx")
+)
+void function() {
+
+}
+
+RAINY_ENABLE_MOC()
+void function1() {
+
+}
+
+RAINY_ENABLE_MOC()
+void function2() {
+
+}
+
+namespace test_namespace {
+    namespace nest {
+        class c {
+        public:
+            RAINY_ENABLE_MOC();
+
+            c() {}
+
+            void foo() {}
+            
+            void foo(int, int) {
+            }
+
+            int mem() {
+            }
+
+            static void bar() {}
+
+            int aa;
+
+        private:
+            int b;
+        };
+    }
+}
