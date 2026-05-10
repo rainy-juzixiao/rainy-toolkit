c80f6d95f0874d342a46dd6618989afa6f2f9997
rainy-juzixiao <aTzXQR8w96I@outlook.com>
2026-05-10 17:22:22 +0800

    5/10/26
    Rollback tss_ptr.hpp

--- xaga/include/rainy/foundation/concurrency/basic/tss_ptr.hpp
+++ xaga/include/rainy/foundation/concurrency/basic/tss_ptr.hpp
@@ -22,19 +22,23 @@ namespace rainy::foundation::concurrency {
     template <typename Ty>
     class tss_ptr {
     public:
-        operator Ty *() {
-            return static_cast<Ty *>(implements::tss_get(get_key()));
+        tss_ptr() : tss_key{implements::tss_create()} {
+        }
+
+        ~tss_ptr() {
+            implements::tss_delete(tss_key);
+        }
+
+        operator Ty *() { // NOLINT
+            return static_cast<Ty*>(implements::tss_get(tss_key));
         }
 
         void operator=(Ty *value) {
-            implements::tss_set(get_key(), value);
+            implements::tss_set(tss_key, value);
         }
 
     private:
-        static core::handle get_key() {
-            static core::handle key = implements::tss_create();
-            return key;
-        }
+        core::handle tss_key;
     };
 }
 
