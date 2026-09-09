a5a74abed2821eb9dddb4e1157465f5e4bd5b934
rainy_juzixiao <aTzXQR8w96I@outlook.com>
2026-09-09 20:00:29 +0800

    fix(core): Fix functor.cc

--- tektite/library/core/tests/functional/functor.cc
+++ tektite/library/core/tests/functional/functor.cc
@@ -16,6 +16,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers.hpp>
 #include <numeric>
+#include <algorithm>
 #include <rainy/core/functional/functor.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
