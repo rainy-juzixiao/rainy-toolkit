3f94c55510cc2a6689b63dc00ad164ba48b5aa34
rainy_juzixiao <aTzXQR8w96I@outlook.com>
2026-09-10 23:09:37 +0800

    feat: Added marco for avoid generate some function documentation.

--- tektite/library/core/include/rainy/core/platform.hpp
+++ tektite/library/core/include/rainy/core/platform.hpp
@@ -4848,9 +4848,13 @@ namespace rainy::utility {
     struct monostate {};
 }
 
+#if !RAINY_HAS_MUZIYAN_REACH_FOR_THE_MOON
+
 namespace rainy::core::implements {
     RAINY_TOOLKIT_API void throw_exception_out_of_range(const char *msg);
     RAINY_TOOLKIT_API void throw_exception_length_error(const char *msg);
 }
+
+#endif
 
 #endif
